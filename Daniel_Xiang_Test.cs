89335e224a7b78a614fd27938ac8e8c3b29007c8
Daniel Xiang <dsxfbz@mst.edu>
2020-10-06 15:14:59 -0500

    Daniel Xiang testing Visual Studio and trying C#.

---
+++ Daniel_Xiang_Test.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace Daniel_Xiang_Test
+{
+    class card
+    {
+        string color;
+        string symbol;
+
+        //string rules;
+
+        bool matches(card target);
+        string getRules();
+    }
+
+    class memMatrix
+    {
+        byte[,] quantity = new byte[5,15];
+
+        string[] colors = {"Red", "Blue", "Yellow", "Green", "Black"};
+        string[] symbols = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "Skip", "Draw Two", "Reverse", "Wild", "Wild Draw 4"};
+        //Can "Wild" and "Wild draw 4" use the same symbol?
+        
+    }
+
+    class DX_Main_Class
+    {
+        safsfascis
+    }
+}
